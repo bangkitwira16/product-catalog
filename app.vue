aba04b2ef9d85726f9c9431c0282696dd80792a0
bangkitwira <bangkitwira@gmail.com>
2022-12-16 21:06:38 +0700

    implement header and cart drawer on app

---
+++ app.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const isCartActive = ref(false);
+</script>
+
+<template>
+  <NuxtLayout>
+    <Header @on-clicked-cart="isCartActive = true" />
+    <NuxtPage />
+    <CartDrawer :is-active="isCartActive" @on-close="isCartActive = false"/>
+  </NuxtLayout>
+</template>
+
+<style lang="scss">
+body {
+  margin: 0;
+}
+</style>
